f74bacd523c6e783a735ef5f94d958c648bad14b
Martin Ebeling <martinebeling@gmx.de>
2021-01-08 19:03:58 +0100

    initial commit for new version

---
+++ bundles/org.openhab.binding.owl/src/main/java/org/openhab/binding/owl/internal/owlConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2021-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.owl.internal;
+
+/**
+ * The {@link owlConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Martin Ebeling - Initial contribution
+ */
+public class owlConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    public String config1;
+}
